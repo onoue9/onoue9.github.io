efd8bcf60458921966ad950f135b22f9e196dffd
onoue9 <filipeflom@gmail.com>
2021-09-29 17:11:29 -0300

    atualizados os arquivos index.html style.css script.js

--- gotquiz/script.js
+++ gotquiz/script.js
@@ -1,5 +1,8 @@
 const startQuizButton = document.querySelector('.start-quiz');
-
+const nextQuizButton = document.createElement('button');
+const points = document.querySelector('.points');
+nextQuizButton.innerText = 'Next'
+let pontos = 0;
 
 function getRandomId() {
   const randomId = Math.floor(Math.random() * 53);
@@ -28,6 +31,8 @@ function wrongAlternative(targetName) {
   const section = document.querySelector('.alternatives-quiz');
   const div = document.querySelectorAll('.alternative');
   const img = document.querySelector('.character-picture');
+  const sectionButton = document.querySelector('.button-quiz');
+  sectionButton.appendChild(nextQuizButton);
   div.forEach((item) => {
     if (item.innerText === targetName) { 
       return item.className = 'wrong-alternative';
@@ -41,13 +46,17 @@ function wrongAlternative(targetName) {
 
 function correctAlternative(targetName) {
   const section = document.querySelector('.alternatives-quiz');
+  const sectionButton = document.querySelector('.button-quiz');
   const div = document.querySelectorAll('.alternative');
+  sectionButton.appendChild(nextQuizButton);
   div.forEach((item) => {
     if (item.innerText === targetName) { 
       item.className = 'correct-alternative';
     };
     if (item.innerText !== targetName) return section.removeChild(item);
   })
+  pontos += 1;
+  points.innerText = `Points: ${pontos}`;
 }
 
 function clickAlternative(target) {
@@ -55,32 +64,41 @@ function clickAlternative(target) {
   return target.target.innerText === img.alt ? correctAlternative(target.target.innerText) : wrongAlternative(target.target.innerText);
 }
 
-function renderAlternative(alternatives) {
-  alternatives.forEach((item) => {
+function renderAlternative(alternatives) {  
+  alternatives.forEach((item) => {      
     const div = document.createElement('div');
     div.innerText = `${item}`;
     div.className = 'alternative';
     div.addEventListener('click', clickAlternative);
     const section = document.querySelector('.alternatives-quiz');
-    section.appendChild(div);    
-  })
+    section.appendChild(div);      
+  }) 
 }
 
 async function setAlternative(character) {
-  const characterTwo = await getInfo();
-  const characterThree = await getInfo();
-  const characterFour = await getInfo();
-  const alternatives = [`${character.firstName} ${character.lastName}`, `${characterTwo.firstName} ${characterTwo.lastName}`, 
-  `${characterThree.firstName} ${characterThree.lastName}`, `${characterFour.firstName} ${characterFour.lastName}`];
-  renderAlternative(alternatives);
+  const characterOne = `${character.firstName} ${character.lastName}`;
+  const characterTwo = `${(await getInfo()).firstName} ${character.lastName}`;
+  const characterThree = `${character.firstName} ${(await getInfo()).lastName}`;
+  const characterFour = `${(await getInfo()).firstName} ${(await getInfo()).lastName}`;
+  const alternatives = [characterOne, characterTwo, characterThree, characterFour];
+  const array = [];
+  for (let index = 0; index < 4; index += 1) {
+    let number = Math.floor(Math.random() * alternatives.length);
+    array.push(alternatives[number]);
+    alternatives.splice(number, 1);    
+  }
+  renderAlternative(array);
 }
 
 function setQuiz(character) {
   const section = document.querySelector('.picture-quiz');
   const img = document.createElement('img');
+  const p = document.createElement('p');
+  p.innerText = 'Which name of this character ?';
   img.src = character.imageUrl;
   img.alt = `${character.firstName} ${character.lastName}`;
   img.className = 'character-picture'
+  section.appendChild(p);
   section.appendChild(img);
   setAlternative(character);
 }
@@ -88,11 +106,28 @@ function setQuiz(character) {
 async function startQuiz() {
   const section = document.querySelector('.button-quiz');  
   const character = await getInfo();  
-  setQuiz(character);
+  setQuiz(character);  
   section.removeChild(section.firstElementChild);
 }
 
+function restartQuiz() {
+  const sectionPicture = document.querySelector('.picture-quiz');
+  const sectionAlternatives = document.querySelector('.alternatives-quiz');
+  const alternatives = document.querySelectorAll('.alternative');
+  const correctAlternative = document.querySelector('.correct-alternative');
+  const wrongAlternative = document.querySelector('.wrong-alternative');
+  sectionPicture.removeChild(sectionPicture.firstElementChild);
+  sectionPicture.removeChild(sectionPicture.firstElementChild);
+  alternatives.forEach((item) => {
+    sectionAlternatives.removeChild(item);
+  })
+  sectionAlternatives.removeChild(correctAlternative);
+  if (wrongAlternative) { sectionAlternatives.removeChild(wrongAlternative); }
+  startQuiz();
+}
+
 startQuizButton.addEventListener('click', startQuiz);
+nextQuizButton.addEventListener('click', restartQuiz);
 
 async function fetchInfo() {
   try {
