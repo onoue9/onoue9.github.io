364e8b9a037fc5565c9f929f402c96796b4b936c
Filipe Onoue <filipeflom@gmail.com>
2022-08-18 17:48:02 -0300

    refactor: :recycle: change the endpoint base in vite.config.js

--- home/vite.config.js
+++ home/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://onoue9.github.io/
+// https://onoue9.github.io/home
 export default defineConfig({
   plugins: [react()]
 })
