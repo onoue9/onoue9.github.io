d0cf04e2350ee2a2552330c3462359bef202d0de
Filipe Onoue <filipeflom@gmail.com>
2022-08-18 17:57:05 -0300

    fix: :adhesive_bandage: fix the base repo

--- home/vite.config.js
+++ home/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://onoue9.github.io/home
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/home/',
+  base: '/onoue9.github.io/',
   plugins: [react()]
 })
