dca87b7c51636f57a06c0276422a954d7f675bff
Filipe Onoue <filipeflom@gmail.com>
2022-08-18 17:39:16 -0300

    feat: :sparkles: change de base on vite.config.js

--- home/vite.config.js
+++ home/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://onoue9.github.io/
 export default defineConfig({
   plugins: [react()]
 })
